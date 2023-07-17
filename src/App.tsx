787ccd13f45bc56f98d0e0444ad7d269d82c2454
Jairo Doni <jdoni@scale.com.br>
2023-07-17 16:32:17 -0300

    Rotas implementadas

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { ThemeProvider } from 'styled-components'
 import { ProductsProvider } from './context/ProductsContext'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <ProductsProvider>
-        <h1>BATATA</h1>
-      </ProductsProvider>
+      <BrowserRouter>
+        <ProductsProvider>
+          <Router />
+        </ProductsProvider>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  flex: 1;
+
+  padding: 0.8rem 2.5rem;
+
+  display: flex;
+  flex-direction: column;
+`
